73952042b8ba881d541e53e1499be1040ee0d218
usermuser <lwrdrstv@gmail.com>
2017-11-12 22:33:04 +0500

    added product model

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+ class Catalog(models.Model):
+ 	name = models.CharField(max_length=255)
+ 	slug = models.SlugFiels(max_length=150)
+ 	description = model.TextField()
+
+ class Product(models.Model):
+ 	name = models.CharField(max_length=300)
+ 	slug = models.SlugFiels(max_length=150)
+ 	description = models.TextField()
+ 	manufacturer = models.CharField(max_length=300, blank=True)
+ 	price = models.DecimalField(max_digits=6,decimal_places=2)
+
+
