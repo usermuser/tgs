8d86f3cc46bcf97d930911b3bb7a3bc54cc7cb2f
usermuser <lwrdrstv@gmail.com>
2017-11-13 00:14:49 +0500

    created index.html,view,url and tested

--- catalog/models.py
+++ catalog/models.py
@@ -1,15 +1,30 @@
 from django.db import models
 
- class Catalog(models.Model):
- 	name = models.CharField(max_length=255)
- 	slug = models.SlugFiels(max_length=150)
- 	description = model.TextField()
+class Catalog(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=150)
+    description = models.TextField()
 
- class Product(models.Model):
- 	name = models.CharField(max_length=300)
- 	slug = models.SlugFiels(max_length=150)
- 	description = models.TextField()
- 	manufacturer = models.CharField(max_length=300, blank=True)
- 	price = models.DecimalField(max_digits=6,decimal_places=2)
+    class Meta:
+        ordering=('name',)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=150)
+    description = models.TextField()
+    manufacturer = models.CharField(max_length=300, blank=True)
+    price = models.DecimalField(max_digits=8,decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    class Meta:
+        ordering=('name',)
+
+    def __str__(self):
+        return self.name
+
+
 
 
