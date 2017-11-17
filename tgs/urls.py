c588d8862720741607190cdc48c7b5cfc1cbb2cb
Tum0xa <timofey.samodurov@yandex.ru>
2017-11-17 09:46:19 +0500

    fixed

--- tgs/urls.py
+++ tgs/urls.py
@@ -20,5 +20,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 #    url(r'^', include('catalog.urls', namespace='catalog')),
-	url(r'^/',include('main.urls')),
+	url(r'^/',include('catalog.urls')),
 ]
