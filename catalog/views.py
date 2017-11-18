5417cc39071e5ce98dfbbf7faa6ffb4fcc5f7806
usermuser <lwrdrstv@gmail.com>
2017-11-19 03:19:04 +0500

    started to add subcategory

--- catalog/views.py
+++ catalog/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .models import Catalog, Product
+#from .models import Category
 
 #def product_list(request):
 #    products =  Product.objects.filter(available=True)
@@ -13,8 +14,10 @@ from .models import Catalog, Product
 
 def index(request):
 	main_categories = Catalog.objects.all()
+	#some
 	template = loader.get_template('catalog/index.html')
 	context = {
 	'main_categories' : main_categories,
+#	'SOME SUBCATEGORY': SUBCATEGORY,
 	}
 	return HttpResponse(template.render(context,request))
