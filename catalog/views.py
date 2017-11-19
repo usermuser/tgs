8723909990e3df8714e01ec222a9bf9bfb113623
usermuser <lwrdrstv@gmail.com>
2017-11-19 23:56:40 +0500

    added subcategories to template

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from .models import Catalog, Product
-#from .models import Category
+from .models import Category
 
 #def product_list(request):
 #    products =  Product.objects.filter(available=True)
@@ -14,8 +14,15 @@ from .models import Catalog, Product
 
 def index(request):
 	main_categories = Catalog.objects.all()
-	#some
+
+
+
+
+
+
+
 	template = loader.get_template('catalog/index.html')
+
 	context = {
 	'main_categories' : main_categories,
 #	'SOME SUBCATEGORY': SUBCATEGORY,
